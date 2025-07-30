57cc8716676c37fd7289630ad6f3d427fdd1ae70
yhw-dev <yhwdev@gmail.com>
2025-07-30 16:31:47 +0900

    feat: 임시 WelcomePage 추가

---
+++ lib/views/pages/welcome_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FittedBox(
+                  child: Text(
+                    'Bet U',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50.0,
+                      letterSpacing: 10.0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                FilledButton(
+                  onPressed: () {},
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text('시작하기'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text('로그인'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

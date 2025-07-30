3ee4cf0bc5a4f1ec395c850c6b4bf3a6af8dbdc3
yhw-dev <yhwdev@gmail.com>
2025-07-30 16:57:19 +0900

    feat: constants, container_widget 추가

---
+++ lib/data/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainTextStyle {
+  static const TextStyle titleGreenText = TextStyle(
+    color: Colors.green,
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle descriptionText = TextStyle(
+    fontSize: 16.0,
+  );
+}
---
+++ lib/views/widgets/container_widget.dart
@@ -0,0 +1,29 @@
+import 'package:bet_u/data/constants.dart';
+import 'package:flutter/material.dart';
+
+class ContainerWidget extends StatelessWidget {
+  const ContainerWidget({super.key, required this.title, required this.description,});
+
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 2.0),
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: MainTextStyle.titleGreenText),
+              Text(description, style: MainTextStyle.descriptionText),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

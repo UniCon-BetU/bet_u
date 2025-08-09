9153e67ad5eaec6eae9fac3df8e8c4b962e2bb11
yeono1220 <yeono1220@gmail.com>
2025-08-09 18:34:06 +0900

    app/build.gradle.kts ndk 버전 고정하려다가 원래 main 상태로 다시 수정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.bet_u"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"  // 직접 지정
+    ndkVersion = flutter.ndkVersion 
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
